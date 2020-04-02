cb101ec89e95dbc969a27e83401614e89034fb79
Lander <Lander@LanderFI>
2020-04-02 11:00:11 +0200

    ModeloTalla java getAll metodoa eginda

--- src/modelo/dao/ModeloTalla.java
+++ src/modelo/dao/ModeloTalla.java
@@ -1,13 +1,33 @@
 package modelo.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
+import modelo.Conector;
 import modelo.bean.Talla;
 
-public class ModeloTalla {
+public class ModeloTalla extends Conector{
 	
 	public ArrayList<Talla> getAll(){
-		//TODO implementar metodo que devuelva todos los estados
-		return null;
+		ArrayList<Talla> tallas = new ArrayList<Talla>();
+		
+		try {
+			Statement st = super.conexion.createStatement();
+			ResultSet rs = st.executeQuery("select * from tallas");
+			while(rs.next()) {
+				Talla talla = new Talla();
+				talla.setId(rs.getInt("id"));
+				talla.setNombre("nombre");
+				
+				tallas.add(talla);
+			}
+			return tallas;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return tallas;
 	}
 }
