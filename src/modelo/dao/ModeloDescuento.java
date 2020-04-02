73d14cfc920dee857f38f66d2aabf9f05a4d75d0
Lander <Lander@LanderFI>
2020-04-02 10:49:23 +0200

    ModeloDescuento java getAll metodoa eginda

--- src/modelo/dao/ModeloDescuento.java
+++ src/modelo/dao/ModeloDescuento.java
@@ -1,14 +1,37 @@
 package modelo.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
+import modelo.Conector;
 import modelo.bean.Descuento;
 
-public class ModeloDescuento {
+public class ModeloDescuento extends Conector{
 	
 	public ArrayList<Descuento> getAll(){
-		//TODO implementar metodo que devuelva todos los estados
-		return null;
+		
+		ArrayList<Descuento> descuentos = new ArrayList<Descuento>();
+		
+		try {
+			Statement st = super.conexion.createStatement();
+			ResultSet rs = st.executeQuery("select * from descuentos");
+			while(rs.next()) {
+				Descuento descuento = new Descuento();
+				descuento.setId(rs.getInt("id"));
+				descuento.setValor(rs.getInt("valor"));
+				descuento.setDescripcion("descripcion");
+				
+				descuentos.add(descuento);
+			}
+			return descuentos;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return descuentos;
 	}
 
 }
