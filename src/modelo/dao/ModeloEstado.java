688a7589f52add5384446a46bc9567a45694f895
Lander <Lander@LanderFI>
2020-04-02 10:55:29 +0200

    ModeloEstado java getAll metodoa eginda

--- src/modelo/dao/ModeloEstado.java
+++ src/modelo/dao/ModeloEstado.java
@@ -1,14 +1,35 @@
 package modelo.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
+import modelo.Conector;
 import modelo.bean.Estado;
 
-public class ModeloEstado {
+public class ModeloEstado extends Conector{
 	
 	public ArrayList<Estado> getAll(){
-		//TODO implementar metodo que devuelva todos los estados
-		return null;
+		ArrayList<Estado> estados = new ArrayList<Estado>();
+		
+		try {
+			Statement st = super.conexion.createStatement();
+			ResultSet rs = st.executeQuery("select * from estados");
+			while(rs.next()) {
+				Estado estado = new Estado();
+				estado.setId(rs.getInt("id"));
+				estado.setName("name");
+				estado.setContinent("continent");
+				
+				estados.add(estado);
+			}
+			return estados;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return estados;
 	}
 
 }
