678130ca9b0d5f02f519120c20e7c3a4c25830e5
Lander <Lander@LanderFI>
2020-04-20 12:47:58 +0200

    ver productos java funtzionatzen

--- src/controladores/VerProductos.java
+++ src/controladores/VerProductos.java
@@ -1,15 +1,12 @@
 package controladores;
 
 import java.io.IOException;
-import java.util.ArrayList;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import modelo.bean.Producto;
 import modelo.dao.ModeloProducto;
 
 /**
@@ -32,10 +29,8 @@ public class VerProductos extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ModeloProducto mProducto = new ModeloProducto();
-		ArrayList<Producto> productos = mProducto.selectAll();
 		
-		request.setAttribute("productos", productos);
-		
+		request.setAttribute("productos", mProducto.selectAll());
 		request.getRequestDispatcher("verProductos.jsp").forward(request, response);
 	}
 
